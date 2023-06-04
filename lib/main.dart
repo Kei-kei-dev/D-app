f86a6703eae3e93fc0048c6efdcfce50b088ccd6
Kei-kei-dev <124547360+Kei-kei-dev@users.noreply.github.com>
2023-06-04 17:34:18 +0900

    スワイプ機能の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/material.dart';
+
 import 'package:d_app/config/app_router.dart';
 import 'package:d_app/screens/screens.dart';
-import 'package:flutter/material.dart';
+import 'package:d_app/blocs/swipe/swipe_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'models/models.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,10 +16,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      onGenerateRoute: AppRouter.onGenerateRoute,
-      initialRoute: HomeScreen.routeName,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => SwipeBloc()..add(LoadUserEvent(users: User.users)),
+        )
+      ],
+      child: const MaterialApp(
+        title: 'Flutter Demo',
+        onGenerateRoute: AppRouter.onGenerateRoute,
+        initialRoute: HomeScreen.routeName,
+      ),
     );
   }
 }
